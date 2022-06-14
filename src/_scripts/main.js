c9ff2a6bdfe289983ac8b692a3e6e4ee97f84254
Dhanfia <dhanfiarahman@gmail.com>
2022-06-14 17:00:00 +0200

    Notications and header fix

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,7 +1,11 @@
 document.addEventListener('click', event => {
-    if (!event.target.matches('.something')) {
+
+    if (!event.target.matches('#notifications-toggler')) {
         return;
     }
+
+    const notificationsPopover = document.getElementById("notifications");
+    notificationsPopover.classList.toggle('OPEN');
 
     console.log('something clicked');
 });
